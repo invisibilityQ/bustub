1aa01dd98ee8c5fc3c1e91d20fdb20a5dd5d6a40
qy <418903362@qq.com>
2023-06-02 13:46:50 +0800

    finish project2

--- src/buffer/buffer_pool_manager_instance.cpp
+++ src/buffer/buffer_pool_manager_instance.cpp
@@ -29,11 +29,6 @@ BufferPoolManagerInstance::BufferPoolManagerInstance(size_t pool_size, DiskManag
   for (size_t i = 0; i < pool_size_; ++i) {
     free_list_.emplace_back(static_cast<int>(i));
   }
-
-  // TODO(students): remove this line after you have implemented the buffer pool manager
-  // throw NotImplementedException(
-  //    "BufferPoolManager is not implemented yet. If you have finished implementing BPM, please remove the throw "
-  //    "exception line in `buffer_pool_manager_instance.cpp`.");
 }
 
 BufferPoolManagerInstance::~BufferPoolManagerInstance() {
@@ -42,73 +37,54 @@ BufferPoolManagerInstance::~BufferPoolManagerInstance() {
   delete replacer_;
 }
 
-// @brief 在缓冲池中创建一个新页面。 将 page_id 设置为新页面的
-// id，如果所有框架当前都在使用中且不可清除（换句话说，固定），则设置为 nullptr。
-//     *
-//     * 您应该从空闲列表或替换器中选择替换框架（总是首先从空闲列表中找到），
-// 然后调用 AllocatePage() 方法来获取新的页面 ID。 如果替换帧有脏页，你应该先把它写回磁盘。
-// 您还需要为新页面重置内存和元数据。
-//     *
-//     * 请记住通过调用 replacer.SetEvictable(frame_id, false)
-//     来“固定”框架，这样替换器就不会在缓冲池管理器“取消固定”它之前驱逐该框架。
-//     另外，请记住在替换器中记录帧的访问历史，以便 lru-k 算法工作。
-//     *
-//     * @param[out] page_id 创建页面的id
-//     * @return nullptr 如果不能创建新页面，否则指向新页面的指针
-//     */
 auto BufferPoolManagerInstance::NewPgImp(page_id_t *page_id) -> Page * {
   std::scoped_lock<std::mutex> lock(latch_);
-  size_t i = 0;
-  for (; i < pool_size_; i++) {
+
+  bool is_free_page = false;
+  for (size_t i = 0; i < pool_size_; i++) {
     if (pages_[i].GetPinCount() == 0) {
+      is_free_page = true;
       break;
     }
   }
-  if (i == pool_size_) {
+
+  if (!is_free_page) {
     return nullptr;
   }
 
-  frame_id_t frame_id;
   *page_id = AllocatePage();
+
+  frame_id_t frame_id;
+
   if (!free_list_.empty()) {
     frame_id = free_list_.front();
     free_list_.pop_front();
-    // }else if (!replacer_->Evict(&frame_id)){
-    //   std::cout<<"replacer->evict false"<<std::endl;
-    //   return nullptr;
   } else {
+    // assert(replacer_->Evict(&frame_id));
     replacer_->Evict(&frame_id);
-    page_id_t evict_page_id = pages_[frame_id].GetPageId();
+    page_id_t evicted_page_id = pages_[frame_id].GetPageId();
+
     if (pages_[frame_id].IsDirty()) {
-      disk_manager_->WritePage(evict_page_id, pages_[frame_id].GetData());
+      disk_manager_->WritePage(evicted_page_id, pages_[frame_id].GetData());
       pages_[frame_id].is_dirty_ = false;
     }
+
     pages_[frame_id].ResetMemory();
-    page_table_->Remove(evict_page_id);
+
+    page_table_->Remove(evicted_page_id);
   }
 
   page_table_->Insert(*page_id, frame_id);
 
-  replacer_->RecordAccess(frame_id);
-  replacer_->SetEvictable(frame_id, false);
-
   pages_[frame_id].page_id_ = *page_id;
   pages_[frame_id].pin_count_ = 1;
+
+  replacer_->RecordAccess(frame_id);
+  replacer_->SetEvictable(frame_id, false);
 
   return &pages_[frame_id];
 }
 
-// @brief 从缓冲池中获取请求的页面。 如果 page_id
-// 需要从磁盘中获取，但所有帧当前都在使用中且不可清除（换句话说，固定），则返回 nullptr。
-//     *
-//     * 首先在缓冲池中查找page_id。 如果没有找到，从空闲列表或替换器中选择一个替换帧（总是首先从空闲列表中找到），
-// 通过调用 disk_manager_->ReadPage() 从磁盘读取页面，并替换帧中的旧页面。
-//  与NewPgImp()类似，如果旧页面脏了，需要将其写回磁盘并更新新页面的元数据
-//     *
-//     * 此外，请记住禁用驱逐并记录框架的访问历史记录，就像您对 NewPgImp() 所做的那样。
-//     *
-//     * @param page_id 要获取的页面的id
-//     * @return nullptr 如果无法获取 page_id，否则指向请求页面的指针
 auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
   std::scoped_lock<std::mutex> lock(latch_);
 
@@ -120,38 +96,40 @@ auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
     return &pages_[frame_id];
   }
 
-  size_t i = 0;
-  for (; i < pool_size_; i++) {
-    if (pages_[i].pin_count_ == 0) {
+  bool is_free_page = false;
+  for (size_t i = 0; i < pool_size_; i++) {
+    if (pages_[i].GetPinCount() == 0) {
+      is_free_page = true;
       break;
     }
   }
-  if (i == pool_size_) {
+
+  if (!is_free_page) {
     return nullptr;
   }
 
   if (!free_list_.empty()) {
     frame_id = free_list_.front();
     free_list_.pop_front();
-    // }
-    // if (!replacer_->Evict(&frame_id)){
-    //   return nullptr;
   } else {
+    // assert(replacer_->Evict(&frame_id));
     replacer_->Evict(&frame_id);
-    page_id_t evict_page_id = pages_[frame_id].GetPageId();
+    page_id_t evicted_page_id = pages_[frame_id].GetPageId();
+
     if (pages_[frame_id].IsDirty()) {
-      disk_manager_->WritePage(evict_page_id, pages_[frame_id].GetData());
+      disk_manager_->WritePage(evicted_page_id, pages_[frame_id].GetData());
       pages_[frame_id].is_dirty_ = false;
     }
+
     pages_[frame_id].ResetMemory();
 
-    page_table_->Remove(evict_page_id);
+    page_table_->Remove(evicted_page_id);
   }
 
   page_table_->Insert(page_id, frame_id);
+
   pages_[frame_id].page_id_ = page_id;
   pages_[frame_id].pin_count_ = 1;
-
   disk_manager_->ReadPage(page_id, pages_[frame_id].GetData());
 
   replacer_->RecordAccess(frame_id);
@@ -160,24 +138,14 @@ auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
   return &pages_[frame_id];
 }
 
-// /**
-//     * TODO(P1): 添加实现
-//     *
-//     * @brief 从缓冲池中取消固定目标页面。 如果 page_id 不在缓冲池中或者其 pin 计数已经为 0，则返回 false。
-//     *
-//     * 减少页面的引脚数。 如果 pin 数达到 0，则该帧应该可以被替换器逐出。
-//     此外，在页面上设置脏标志以指示页面是否被修改。
-//     *
-//     * @param page_id 要取消固定的页面的 id
-//     * @param is_dirty 如果页面应该被标记为脏，则为 true，否则为 false
-//     * @return false 如果页面不在页表中或者在调用之前它的 pin 计数 <= 0，否则为 true
-//     */
 auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> bool {
   std::scoped_lock<std::mutex> lock(latch_);
+
   frame_id_t frame_id;
-  if (page_id == INVALID_PAGE_ID || !page_table_->Find(page_id, frame_id)) {
+  if (!page_table_->Find(page_id, frame_id)) {
     return false;
   }
+
   if (pages_[frame_id].GetPinCount() <= 0) {
     return false;
   }
@@ -187,36 +155,26 @@ auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> 
   }
 
   pages_[frame_id].pin_count_--;
-  if (pages_[frame_id].GetPinCount() == 0) {
+
+  if (pages_[frame_id].pin_count_ == 0) {
     replacer_->SetEvictable(frame_id, true);
   }
 
   return true;
 }
 
-/**
- * TODO(P1): 添加实现
- *
- * @brief 将目标页面刷新到磁盘。
- *
- * 使用 DiskManager::WritePage() 方法将页面刷新到磁盘，不管是否有脏标志。
- * 刷新后取消设置页面的脏标志。
- *
- * @param page_id 要刷新的页面的id，不能是INVALID_PAGE_ID
- * @return false 如果在页表中找不到该页，否则为 true
- */
 auto BufferPoolManagerInstance::FlushPgImp(page_id_t page_id) -> bool {
-  std::scoped_lock<std::mutex> lock(latch_);
-  if (page_id == INVALID) {
+  if (page_id == INVALID_PAGE_ID) {
     return false;
   }
+
   frame_id_t frame_id;
+
   if (!page_table_->Find(page_id, frame_id)) {
     return false;
   }
 
-  disk_manager_->WritePage(page_id, pages_[frame_id].GetData());
-  // pages_[frame_id].is_dirty_ = false;
+  disk_manager_->WritePage(page_id, pages_[frame_id].data_);
   return true;
 }
 
@@ -229,25 +187,27 @@ void BufferPoolManagerInstance::FlushAllPgsImp() {
 
 auto BufferPoolManagerInstance::DeletePgImp(page_id_t page_id) -> bool {
   std::scoped_lock<std::mutex> lock(latch_);
+
   frame_id_t frame_id;
-  if (!page_table_->Find(page_id, frame_id) || page_id == INVALID_PAGE_ID) {
+  if (!page_table_->Find(page_id, frame_id)) {
     return true;
   }
 
   if (pages_[frame_id].GetPinCount() > 0) {
     return false;
   }
+
   replacer_->Remove(frame_id);
 
   pages_[frame_id].ResetMemory();
+  pages_[frame_id].page_id_ = INVALID_PAGE_ID;
   pages_[frame_id].pin_count_ = 0;
   pages_[frame_id].is_dirty_ = false;
-  pages_[frame_id].page_id_ = INVALID_PAGE_ID;
 
   page_table_->Remove(page_id);
-  free_list_.emplace_back(frame_id);
-
+  free_list_.push_back(frame_id);
   DeallocatePage(page_id);
+
   return true;
 }
 
--- src/container/hash/extendible_hash_table.cpp
+++ src/container/hash/extendible_hash_table.cpp
@@ -22,7 +22,8 @@
 namespace bustub {
 
 template <typename K, typename V>
-ExtendibleHashTable<K, V>::ExtendibleHashTable(size_t bucket_size) : bucket_size_(bucket_size) {
+ExtendibleHashTable<K, V>::ExtendibleHashTable(size_t bucket_size)
+    : global_depth_(0), bucket_size_(bucket_size), num_buckets_(1) {
   dir_.push_back(std::make_shared<Bucket>(bucket_size, 0));
 }
 
@@ -68,43 +69,45 @@ auto ExtendibleHashTable<K, V>::GetNumBucketsInternal() const -> int {
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Find(const K &key, V &value) -> bool {
   std::scoped_lock<std::mutex> lock(latch_);
-  size_t bucket_idx = IndexOf(key);
-  return dir_[bucket_idx]->Find(key, value);
+
+  auto index = IndexOf(key);
+  auto target_bucket = dir_[index];
+
+  return target_bucket->Find(key, value);
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Remove(const K &key) -> bool {
   std::scoped_lock<std::mutex> lock(latch_);
-  size_t bucket_idx = IndexOf(key);
-  return dir_[bucket_idx]->Remove(key);
-}
 
-template <typename K, typename V>
-void ExtendibleHashTable<K, V>::Grow() {
-  int capacity = dir_.size();
-  dir_.resize(capacity << 1);
-  for (int i = 0; i < capacity; i++) {  // 调整索引
-    dir_[i + capacity] = dir_[i];
-  }
-  global_depth_++;
+  auto index = IndexOf(key);
+  auto target_bucket = dir_[index];
+
+  return target_bucket->Remove(key);
 }
 
 template <typename K, typename V>
 void ExtendibleHashTable<K, V>::Insert(const K &key, const V &value) {
   std::scoped_lock<std::mutex> lock(latch_);
-  while (dir_[IndexOf(key)]->IsFull()) {  // 需要递归判断是否能将要插入的桶是否为满
-    size_t index = IndexOf(key);
+
+  while (dir_[IndexOf(key)]->IsFull()) {
+    auto index = IndexOf(key);
     auto target_bucket = dir_[index];
-    int bucket_localdepth = target_bucket->GetDepth();
-    if (global_depth_ == bucket_localdepth) {
-      Grow();
+
+    if (target_bucket->GetDepth() == GetGlobalDepthInternal()) {
+      global_depth_++;
+      int capacity = dir_.size();
+      dir_.resize(capacity << 1);
+      for (int i = 0; i < capacity; i++) {
+        dir_[i + capacity] = dir_[i];
+      }
     }
-    int mask = 1 << bucket_localdepth;
-    // 声明两个桶，废除原来目录中指向的那个桶。
-    auto bucket_0 = std::make_shared<Bucket>(bucket_size_, bucket_localdepth + 1);
-    auto bucket_1 = std::make_shared<Bucket>(bucket_size_, bucket_localdepth + 1);
-    // 重新调整桶中数据
-    for (auto &item : target_bucket->GetItems()) {
+
+    int mask = 1 << target_bucket->GetDepth();
+    auto bucket_0 = std::make_shared<Bucket>(bucket_size_, target_bucket->GetDepth() + 1);
+    auto bucket_1 = std::make_shared<Bucket>(bucket_size_, target_bucket->GetDepth() + 1);
+
+    for (const auto &item : target_bucket->GetItems()) {
       size_t hash_key = std::hash<K>()(item.first);
       if ((hash_key & mask) != 0U) {
         bucket_1->Insert(item.first, item.second);
@@ -112,21 +115,30 @@ void ExtendibleHashTable<K, V>::Insert(const K &key, const V &value) {
         bucket_0->Insert(item.first, item.second);
       }
     }
+
     num_buckets_++;
-    // 调整桶中数据
+
     for (size_t i = 0; i < dir_.size(); i++) {
       if (dir_[i] == target_bucket) {
-        if ((i & mask) == 0U) {
-          dir_[i] = bucket_0;
-        } else {
+        if ((i & mask) != 0U) {
           dir_[i] = bucket_1;
+        } else {
+          dir_[i] = bucket_0;
         }
       }
     }
   }
-  // 将数据插入桶中
+
   auto index = IndexOf(key);
   auto target_bucket = dir_[index];
+
+  for (auto &item : target_bucket->GetItems()) {
+    if (item.first == key) {
+      item.second = value;
+      return;
+    }
+  }
+
   target_bucket->Insert(key, value);
 }
 
@@ -138,51 +150,32 @@ ExtendibleHashTable<K, V>::Bucket::Bucket(size_t array_size, int depth) : size_(
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Find(const K &key, V &value) -> bool {
-  for (auto &item : list_) {
-    if (key == item.first) {
-      value = item.second;
-      return true;
-    }
-  }
-  return false;
-}
-
-template <typename K, typename V>
-auto ExtendibleHashTable<K, V>::Bucket::Remove(const K &key) -> bool {
-  if (list_.empty()) {
-    return false;  // 空桶
-  }
-  return std::any_of(list_.begin(), list_.end(), [&key, this](const auto &item) {
-    // for (auto &item : list_) {
+  return std::any_of(list_.begin(), list_.end(), [&key, &value](const auto &item) {
     if (item.first == key) {
-      this->list_.remove(item);
-      // latch_.unlock();
+      value = item.second;
       return true;
     }
     return false;
   });
-  return false;
+}
+
+template <typename K, typename V>
+auto ExtendibleHashTable<K, V>::Bucket::Remove(const K &key) -> bool {
+  return std::any_of(list_.begin(), list_.end(), [&key, this](const auto &item) {
+    if (item.first == key) {
+      this->list_.remove(item);
+      return true;
+    }
+    return false;
+  });
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Insert(const K &key, const V &value) -> bool {
-  // latch_.lock();
   if (IsFull()) {
-    // latch_.unlock();
     return false;
   }
-
-  // 正常插入
-  for (auto &item : list_) {
-    if (item.first == key) {
-      item.second = value;
-      // latch_.unlock();
-      return true;
-    }
-  }
-
-  list_.push_back(std::make_pair(key, value));
-  // latch_.unlock();
+  list_.emplace_back(key, value);
   return true;
 }
 
